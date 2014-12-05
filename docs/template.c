98db0a10f3834256259c47b2d987a13563e1f39b
C-D-Lewis <Bonsitm@gmail.com>
2014-12-05 15:24:23 -0800

    Add template starter file to docs

---
+++ docs/template.c
@@ -0,0 +1,33 @@
+/**
+ * This is the bare minimum to make a looping game with PGE!
+ */
+
+#include <pebble.h>
+#include "pge/pge.h"
+
+static Window *s_game_window;
+
+static void game_logic() {
+  // Per-frame game logic here
+
+}
+
+static void game_draw(GContext *ctx) {
+  // Per-frame game rendering here
+
+}
+
+static void game_click(int button_id) {
+  // Process click events
+
+}
+
+void pge_init() {
+  // Start the game, keep a Window reference for later
+  s_game_window = pge_begin(game_logic, game_draw, game_click);
+}
+
+void pge_deinit() {
+  // Finish the game
+  pge_finish();
+}
