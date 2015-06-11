0f50538ca4160936a95e990603bfe9dcfba2fed1
C-D-Lewis <Bonsitm@gmail.com>
2015-06-10 22:55:51 -0700

    Begin server

---
+++ ws-server/app.js
@@ -0,0 +1,38 @@
+// Require
+var WebSocketServer = require('ws').Server;
+
+// Config
+var DEBUG = true;
+var PORT = 5000;
+
+/********************************* Helper *************************************/
+
+function Log(message) {
+  if(DEBUG) console.log(message);
+}
+
+/********************************* Server *************************************/
+
+var server = new WebSocketServer({ 'port': PORT });
+
+server.on('connection', function (socket) {
+  onClientConnected(socket);
+  socket.on('message', onClientMessage);
+
+Log('Server ready on port ' + PORT);
+
+/******************************* Developer Implementation *********************/
+
+/**
+ * Set up new client
+ */
+function onClientConnected(socket) {
+
+}
+
+/**
+ * React to message from client
+ */
+function onClientMessage(message) {
+
+}
